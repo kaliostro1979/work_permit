7cf8b37ddc0742582a0e142d4a2faf67a98d8402
Misha <abachyanm@gmail.com>
2020-06-25 19:55:46 +0400

    Fix

--- js/main.js
+++ js/main.js
@@ -4,9 +4,17 @@ $('.selected').on('click', function () {
     $('.select-box').toggleClass('box-open')
 });
 
-$('.lang-item').each((i, o)=>{
+$('.lang-item a').each((i, o)=>{
     $(o).on('click', function () {
-        $('.selected-lang').html($(this).html());
+        $('.selected-lang').html($(this).parent().html());
         $('.select-box').removeClass('box-open')
     })
 });
+
+//Collapse FAQ
+
+$('.faq-col-title').on('click', function () {
+    $(this).toggleClass('faq-col-title-open');
+    $(this).find('.faq-icon').toggleClass('faq-open');
+    $(this).parent().find('.faq-col-body').toggleClass('faq-col-body-open')
+})

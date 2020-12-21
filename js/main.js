3453ed266d5fb3b94bb9e2ce22ff86d5e1aacac3
Misha <abachyanm@gmail.com>
2020-12-21 22:40:00 +0400

    Link to mark added

--- js/main.js
+++ js/main.js
@@ -79,14 +79,14 @@ $(".pb-slider").owlCarousel({
 
 function initMap() {
     const myLatLng = { lat: 40.198964680048235, lng: 44.52889812443517 };
-    const map = new google.maps.Map(document.getElementById("map"), {
+    const map = new maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: myLatLng,
     });
     new google.maps.Marker({
         position: myLatLng,
         map:map,
-        icon: 'http://localhost:63342/work_permit/img/icons/marp_mark.png'
+        icon: 'http://work-permit.frigatestudio.com/img/icons/marp_mark.png'
     });
 }
 
